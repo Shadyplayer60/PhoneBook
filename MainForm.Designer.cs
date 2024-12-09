a73fb14b79aa6ce462381dc507718acc92654bc2
tharvey60 <125995743+tharvey60@users.noreply.github.com>
2024-12-09 13:46:52 -0500

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace PhoneBook
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridViewPersons = new System.Windows.Forms.DataGridView();
+            this.personIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phoneNumberDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.personBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.personDataSet = new PhoneBook.PersonDataSet();
+            this.personTableAdapter = new PhoneBook.PersonDataSetTableAdapters.PersonTableAdapter();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPhoneNumber = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.phoneTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPersons)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewPersons
+            // 
+            this.dataGridViewPersons.AllowUserToAddRows = false;
+            this.dataGridViewPersons.AllowUserToDeleteRows = false;
+            this.dataGridViewPersons.AutoGenerateColumns = false;
+            this.dataGridViewPersons.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPersons.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.personIDDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.phoneNumberDataGridViewTextBoxColumn});
+            this.dataGridViewPersons.DataSource = this.personBindingSource;
+            this.dataGridViewPersons.Location = new System.Drawing.Point(12, 3);
+            this.dataGridViewPersons.Name = "dataGridViewPersons";
+            this.dataGridViewPersons.ReadOnly = true;
+            this.dataGridViewPersons.RowHeadersWidth = 51;
+            this.dataGridViewPersons.RowTemplate.Height = 24;
+            this.dataGridViewPersons.Size = new System.Drawing.Size(542, 312);
+            this.dataGridViewPersons.TabIndex = 0;
+            // 
+            // personIDDataGridViewTextBoxColumn
+            // 
+            this.personIDDataGridViewTextBoxColumn.DataPropertyName = "PersonID";
+            this.personIDDataGridViewTextBoxColumn.HeaderText = "PersonID";
+            this.personIDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.personIDDataGridViewTextBoxColumn.Name = "personIDDataGridViewTextBoxColumn";
+            this.personIDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.personIDDataGridViewTextBoxColumn.Visible = false;
+            this.personIDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+            this.nameDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // phoneNumberDataGridViewTextBoxColumn
+            // 
+            this.phoneNumberDataGridViewTextBoxColumn.DataPropertyName = "Phone Number";
+            this.phoneNumberDataGridViewTextBoxColumn.HeaderText = "Phone Number";
+            this.phoneNumberDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.phoneNumberDataGridViewTextBoxColumn.Name = "phoneNumberDataGridViewTextBoxColumn";
+            this.phoneNumberDataGridViewTextBoxColumn.ReadOnly = true;
+            this.phoneNumberDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // personBindingSource
+            // 
+            this.personBindingSource.DataMember = "Person";
+            this.personBindingSource.DataSource = this.personDataSet;
+            // 
+            // personDataSet
+            // 
+            this.personDataSet.DataSetName = "PersonDataSet";
+            this.personDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // personTableAdapter
+            // 
+            this.personTableAdapter.ClearBeforeFill = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(541, 408);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(120, 30);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "&Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(415, 408);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(120, 30);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "&Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(667, 407);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(120, 30);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "&Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 375);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(44, 16);
+            this.lblName.TabIndex = 4;
+            this.lblName.Text = "&Name";
+            // 
+            // lblPhoneNumber
+            // 
+            this.lblPhoneNumber.AutoSize = true;
+            this.lblPhoneNumber.Location = new System.Drawing.Point(163, 375);
+            this.lblPhoneNumber.Name = "lblPhoneNumber";
+            this.lblPhoneNumber.Size = new System.Drawing.Size(97, 16);
+            this.lblPhoneNumber.TabIndex = 5;
+            this.lblPhoneNumber.Text = "&Phone Number";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(15, 407);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(123, 22);
+            this.nameTextBox.TabIndex = 6;
+            // 
+            // phoneTextBox
+            // 
+            this.phoneTextBox.Location = new System.Drawing.Point(166, 407);
+            this.phoneTextBox.Name = "phoneTextBox";
+            this.phoneTextBox.Size = new System.Drawing.Size(142, 22);
+            this.phoneTextBox.TabIndex = 7;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.phoneTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.lblPhoneNumber);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dataGridViewPersons);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPersons)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewPersons;
+        private PersonDataSet personDataSet;
+        private System.Windows.Forms.BindingSource personBindingSource;
+        private PersonDataSetTableAdapters.PersonTableAdapter personTableAdapter;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPhoneNumber;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox phoneTextBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn personIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phoneNumberDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PhoneBook
+{
+    public partial class MainForm : Form
+    {
+        private BindingSource bindingSourceControl;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // Initialize BindingSource
+            bindingSourceControl = new BindingSource();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Reload the dataset from the database
+                ReloadData();
+
+                // Bind BindingSource to the dataset
+                bindingSourceControl.DataSource = personDataSet.Person;
+
+                // Bind DataGridView to BindingSource
+                dataGridViewPersons.DataSource = bindingSourceControl;
+
+                // Bind TextBoxes to BindingSource
+                nameTextBox.DataBindings.Add("Text", bindingSourceControl, "Name");
+                phoneTextBox.DataBindings.Add("Text", bindingSourceControl, "Phone Number");
+
+                // Disable adding new rows directly in the DataGridView
+                dataGridViewPersons.AllowUserToAddRows = false;
+
+                // Hide the PersonID column in DataGridView
+                if (dataGridViewPersons.Columns["PersonID"] != null)
+                {
+                    dataGridViewPersons.Columns["PersonID"].Visible = false;
+                }
+
+                // Initial sort by last name
+                SortByLastName();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading data: {ex.Message}");
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Check for unsaved changes
+            if (personDataSet.Person.GetChanges() != null)
+            {
+                var result = MessageBox.Show(
+                    "You have unsaved changes. Would you like to save them before exiting?",
+                    "Unsaved Changes",
+                    MessageBoxButtons.YesNoCancel,
+                    MessageBoxIcon.Warning);
+
+                if (result == DialogResult.Yes)
+                {
+                    btnSave_Click(null, null); // Save changes
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    e.Cancel = true; // Cancel the form close
+                }
+                else
+                {
+                    ReloadData(); // Discard changes
+                }
+            }
+        }
+
+        private void ReloadData()
+        {
+            try
+            {
+                // Refill the dataset to discard any unsaved changes
+                this.personTableAdapter.Fill(this.personDataSet.Person);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error reloading data: {ex.Message}");
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get the next Id for the new row
+                int nextId = personDataSet.Person.AsEnumerable()
+                              .Select(row => row.Field<int>("PersonID"))
+                              .DefaultIfEmpty(0)
+                              .Max() + 1;
+
+                // Add a blank row to the dataset
+                DataRow newRow = personDataSet.Person.NewRow();
+                newRow["PersonID"] = nextId;
+                newRow["Name"] = ""; // Leave Name blank
+                newRow["Phone Number"] = ""; // Leave Phone Number blank
+                personDataSet.Person.Rows.Add(newRow);
+
+                // Position the BindingSource to the new row
+                bindingSourceControl.Position = bindingSourceControl.Count - 1;
+
+                // Set focus to the Name TextBox for immediate editing
+                nameTextBox.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error adding contact: {ex.Message}");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (bindingSourceControl.Current != null)
+                {
+                    // Remove the selected row from the dataset
+                    bindingSourceControl.RemoveCurrent();
+                }
+                else
+                {
+                    MessageBox.Show("No contact selected to delete.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error deleting contact: {ex.Message}");
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveChanges();
+        }
+
+        private void SaveChanges()
+        {
+            try
+            {
+                // Commit pending changes in the BindingSource to the DataTable
+                bindingSourceControl.EndEdit();
+
+                // Save changes to the database
+                int rowsAffected = personTableAdapter.Update(personDataSet.Person);
+
+                // Notify the user
+                MessageBox.Show($"{rowsAffected} changes saved successfully.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error saving data: {ex.Message}");
+            }
+        }
+
+        private void SortByLastName()
+        {
+            try
+            {
+                DataTable table = personDataSet.Person;
+
+                // Clone the original table structure
+                DataTable sortedTable = table.Clone();
+
+                // Sort rows by the first letter of the last name
+                var sortedRows = table.AsEnumerable()
+                    .OrderBy(row =>
+                    {
+                        string name = row.Field<string>("Name") ?? string.Empty;
+                        var parts = name.Split(' ');
+                        return parts.Length > 1 ? parts[1] : name;
+                    });
+
+                // Import sorted rows into the cloned table
+                foreach (var row in sortedRows)
+                {
+                    sortedTable.ImportRow(row);
+                }
+
+                // Clear the original table and re-import sorted rows
+                table.Clear();
+                foreach (DataRow row in sortedTable.Rows)
+                {
+                    table.ImportRow(row);
+                }
+
+                // Rebind the updated table to the BindingSource
+                bindingSourceControl.DataSource = table;
+
+                // Ensure DataGridView reflects the new binding
+                dataGridViewPersons.DataSource = bindingSourceControl;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error sorting data: {ex.Message}");
+            }
+        }
+    }
+}
